c9f971d72f07297a52cc0e76a4530c283457b622
Luiz <luiz.s08@aluno.fsc.edu.br>
2023-03-06 13:40:37 -0300

    CB Site

---
+++ SiteCB/script.js
@@ -0,0 +1,7 @@
+let toggle = document.getElementById("cbMudaFundo");
+
+if(toggle.value == "on"){
+    document.body.style.backgroundColor = "blue";
+} else {
+    document.body.style.backgroundColor = "black";
+}
