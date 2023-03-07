0c2aa90914dc31217bf2a86cf4f6e2f056d70dc7
Luiz F <luiz.s08@aluno.ifsc.edu.br>
2023-03-07 05:21:42 -0300

    sla

---
+++ SiteCB/mobile-navbar.js
@@ -0,0 +1,39 @@
+class MobileNavbar {
+    constructor() {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll();
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+        })
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new mobileNavbar (
+    ".mobile-menu", 
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
