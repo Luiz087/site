8f36880680f05c26f5f114ace008e8fc1847b176
Luiz F <luiz.s08@aluno.ifsc.edu.br>
2023-03-06 22:41:17 -0300

    BG troca cor

--- SiteCB/script.js
+++
@@ -1,7 +0,0 @@
-let toggle = document.getElementById("cbMudaFundo");
-
-if(toggle.value == "on"){
-    document.body.style.backgroundColor = "blue";
-} else {
-    document.body.style.backgroundColor = "black";
-}
